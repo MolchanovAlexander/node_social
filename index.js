5f92738ea1a52215b30d5738b1a2600ca47b8c0a
Olexander <rybkolhoz@gmail.com>
2023-08-26 22:49:32 +0300

    211

--- index.js
+++ index.js
@@ -1,29 +1,48 @@
-const express= require("express")
-const mongoose= require("mongoose")
-const dotenv= require("dotenv")
-const helmet= require("helmet")
-const morgan= require("morgan")
+const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const helmet = require("helmet")
+const morgan = require("morgan")
 const app = express()
 const userRoute = require("./routes/users")
 const authRoute = require("./routes/auth")
 const postRoute = require("./routes/posts")
+const multer = require("multer")
 
 dotenv.config()
 
 //process.env.MONGO_URL
 mongoose.connect(process.env.MONGO_URL,
-{ useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log('Connected')).catch((err) => console.log(err))
+    { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log('Connected')).catch((err) => console.log(err))
 
-//middleware
+//middleware req.body.name
 app.use(express.json())
 app.use(helmet())
 app.use(morgan("common"))
 
-app.use("/api/users",userRoute)
-app.use("/api/auth",authRoute)
-app.use("/api/posts",postRoute)
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'C:/MolchanovCode/node_social/public/uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    }
+})
+
+const upload = multer({ storage })
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    
+    try {
+        return res.status(200).json("File uploaded")
+    } catch (err) {
+        console.log(err);
+    }})
+
+app.use("/api/users", userRoute)
+app.use("/api/auth", authRoute)
+app.use("/api/posts", postRoute)
 
 
-app.listen(8800,() => {
+app.listen(8800, () => {
     console.log("server runing");
 })
